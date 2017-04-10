940b7bf7d33df53af3ee2a29a9f35b128ddb54bd
Chad Fennell <fenne035@umn.edu>
2017-04-10 10:50:59 -0500

    remove thumbnail class as requested

--- src/transcript-nav.js
+++ src/transcript-nav.js
@@ -12,8 +12,8 @@ export default class TranscriptNav extends React.Component {
           return (
                     <div className="row image-nav">
                       <ul className="nav nav-pills">
-                        <li><Link className="thumbnail" activeClassName="active" to={asset_path}>{transcript.label}</Link></li>
-                        <li ><Link className="thumbnail" activeClassName="active" to={transcript_path}>Transcript</Link></li>
+                        <li><Link className="transcript-nav-link" activeClassName="active" to={asset_path}>{transcript.label}</Link></li>
+                        <li ><Link className="transcript-nav-link" activeClassName="active" to={transcript_path}>Transcript</Link></li>
                       </ul>
                     </div>
                   )
