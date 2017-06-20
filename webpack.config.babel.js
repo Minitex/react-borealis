abe49a3f91aadc2c566b9030e64d9f0f82e3ee94
chadfennell <libsys@gmail.com>
2017-06-20 15:39:20 -0700

    simplify webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,16 +1,12 @@
-var webpack = require("webpack");
-var path = require('path');
-var merge = require('webpack-merge')
-
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
 var TARGET = process.env.npm_lifecycle_event;
-var minimize = (process.argv.indexOf('--minimize') > 0) ? true : false;
-var example =  (process.argv.indexOf('--example') > 0) ? true : false;
-var build =    (process.argv.indexOf('--build') > 0) ? true : false;
- 
-var common = {
+
+let common = {
   module: {
     loaders: [
-      { 
+      {
         test: /\.js?$/,
         loader: 'react-hot-loader',
         include: path.join(__dirname, 'src')
@@ -20,62 +16,58 @@ var common = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ["react", "es2015", "stage-0"]
-        }
+          presets: ['react', 'es2015', 'stage-0'],
+        },
       },
       {
-          test: /\.css$/,
-          loaders: ["style-loader", "css-loader"]
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
       },
       {
-          test: /\.less$/,
-          loaders: ["style-loader", "css-loader"]
-      }
-    ]
+        test: /\.less$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
-      new webpack.HotModuleReplacementPlugin()
-  ]
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
 
-if (build) {
-  if (minimize) {
-    common.plugins.push(new webpack.optimize.UglifyJsPlugin());    
-    var config = 
-    {
-      entry: './src/react-borealis.js',
-      output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'react-borealis-min.js',
-        library: "react-borealis",
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-      }
-    }
-  } else {
-    var config = 
-    {
-      devtool: 'source-map',
-      entry: './src/react-borealis.js',
-      output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'react-borealis.js',
-        library: "react-borealis",
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-      }
-    }
-  }
-  module.exports = merge(common, config);
+if (TARGET !== 'start') {
+  common.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  const minConfig = merge(common, {
+    entry: './src/react-borealis.js',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'react-borealis-min.js',
+      library: 'ReactBorealis',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+    },
+  });
+
+  const mapConfig = merge(common, {
+    devtool: 'source-map',
+    entry: './src/react-borealis.js',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'react-borealis.js',
+      library: 'ReactBorealis',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+    },
+  });
+  module.exports = [minConfig, mapConfig]
 }
 
-if (example || TARGET === 'start') {
+if (TARGET === 'start') {
   module.exports = merge(common, {
     devtool: 'source-map',
     entry: './example/example.js',
     output: {
-        path: path.join(__dirname, 'docs'),
-        filename: "example.js"
-    }
+      path: path.join(__dirname, 'docs'),
+      filename: 'example.js',
+    },
   });
 }
