7d878caddf3237770e12c480494e1136cef2a86f
chadfennell <libsys@gmail.com>
2017-06-21 11:17:13 -0700

    upgrade to latest webpack; more webpack conf refactoring

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,31 +1,32 @@
 const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
-var TARGET = process.env.npm_lifecycle_event;
 
 let common = {
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js?$/,
-        loader: 'react-hot-loader',
-        include: path.join(__dirname, 'src')
+        use: 'react-hot-loader',
+        include: path.join(__dirname, 'src'),
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015', 'stage-0'],
+          },
         },
       },
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
+        use: 'css-loader',
       },
       {
         test: /\.less$/,
-        loaders: ['style-loader', 'css-loader'],
+        use: 'css-loader',
       },
     ],
   },
@@ -34,40 +35,35 @@ let common = {
   ],
 };
 
-if (TARGET !== 'start') {
-  common.plugins.push(new webpack.optimize.UglifyJsPlugin());
-  const minConfig = merge(common, {
-    entry: './src/react-borealis.js',
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'react-borealis-min.js',
-      library: 'ReactBorealis',
-      libraryTarget: 'umd',
-      umdNamedDefine: true,
-    },
-  });
+common.plugins.push(new webpack.optimize.UglifyJsPlugin());
+const minConfig = merge(common  , {
+  entry: './src/react-borealis.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'react-borealis-min.js',
+    library: 'react-borealis',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+});
+const mapConfig = merge(common, {
+  devtool: 'source-map',
+  entry: './src/react-borealis.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'react-borealis.js',
+    library: 'react-borealis',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+});
 
-  const mapConfig = merge(common, {
-    devtool: 'source-map',
-    entry: './src/react-borealis.js',
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'react-borealis.js',
-      library: 'ReactBorealis',
-      libraryTarget: 'umd',
-      umdNamedDefine: true,
-    },
-  });
-  module.exports = [minConfig, mapConfig]
-}
+const exampleConfig = merge(common, {
+  devtool: 'source-map',
+  entry: './example/example.js',
+  output: {
+    filename: 'example.js',
+  },
+});
 
-if (TARGET === 'start') {
-  module.exports = merge(common, {
-    devtool: 'source-map',
-    entry: './example/example.js',
-    output: {
-      path: path.join(__dirname, 'docs'),
-      filename: 'example.js',
-    },
-  });
-}
+module.exports = [minConfig, mapConfig, exampleConfig]
