fad1decb6de943624ff973c919a7d9f47fbee661
chadfennell <libsys@gmail.com>
2017-06-26 10:08:46 -0700

    parens not needed here for single line arrow func

--- src/react-borealis-route.js
+++ src/react-borealis-route.js
@@ -85,24 +85,24 @@ class Borealis extends React.Component {
     return (
       <div>
         <Switch>
-          <Redirect from="/" exact  to={`/${initialPath()}`} />
-          <Route exact path="/image/:id" render={(props) => layout(props, ReactOpenseadragon, 'image')} />
-          <Route exact path="/kaltura_audio" render={(props) => layout(props, KalturaPlayer, 'kaltura_audio')} />
-          <Route exact path="/kaltura_audio_playlist" render={(props) => layout(props, KalturaPlayer, 'kaltura_audio_playlist')} />
-          <Route exact path="/kaltura_video" render={(props) => layout(props, KalturaPlayer, 'kaltura_video')} />
-          <Route exact path="/audio" render={(props) => layout(props, BorealisAudioPlayer, 'audio')} />
-          <Route exact path="/video" render={(props) => layout(props, BorealisVideoPlayer, 'video')} />
-          <Route exact path="/pdf/:id" render={(props) => layout(props, BorealisPDF, 'pdf')} />
-          <Route exact path="/ppt" render={(props) => layout(props, BorealisPPT, 'ppt')}  />
+          <Redirect from="/" exact to={`/${initialPath()}`} />
+          <Route exact path="/image/:id" render={props => layout(props, ReactOpenseadragon, 'image')} />
+          <Route exact path="/kaltura_audio" render={props => layout(props, KalturaPlayer, 'kaltura_audio')} />
+          <Route exact path="/kaltura_audio_playlist" render={props => layout(props, KalturaPlayer, 'kaltura_audio_playlist')} />
+          <Route exact path="/kaltura_video" render={props => layout(props, KalturaPlayer, 'kaltura_video')} />
+          <Route exact path="/audio" render={props => layout(props, BorealisAudioPlayer, 'audio')} />
+          <Route exact path="/video" render={props => layout(props, BorealisVideoPlayer, 'video')} />
+          <Route exact path="/pdf/:id" render={props => layout(props, BorealisPDF, 'pdf')} />
+          <Route exact path="/ppt" render={props => layout(props, BorealisPPT, 'ppt')}  />
 
-          <Route path="/image/:id/transcript" render={(props) => layout(props, BorealisTranscript, 'image')} />
-          <Route path="/kaltura_audio/transcript" render={(props) => layout(props, BorealisTranscript, 'kaltura_audio')} />
-          <Route path="/kaltura_audio_playlist/transcript" render={(props) => layout(props, BorealisTranscript, 'kaltura_audio_playlist')} />
-          <Route path="/kaltura_video/transcript" render={(props) => layout(props, BorealisTranscript, 'kaltura_video')} />
-          <Route path="/audio/transcript" render={(props) => layout(props, BorealisTranscript, 'audio')} />
-          <Route path="/video/transcript" render={(props) => layout(props, BorealisTranscript, 'video')} />
-          <Route path="/pdf/:id/transcript" render={(props) => layout(props, BorealisTranscript, 'pdf')} />
-          <Route path="/ppt/transcript" render={(props) => layout(props, BorealisTranscript, 'ppt')} />
+          <Route path="/image/:id/transcript" render={props => layout(props, BorealisTranscript, 'image')} />
+          <Route path="/kaltura_audio/transcript" render={props => layout(props, BorealisTranscript, 'kaltura_audio')} />
+          <Route path="/kaltura_audio_playlist/transcript" render={props => layout(props, BorealisTranscript, 'kaltura_audio_playlist')} />
+          <Route path="/kaltura_video/transcript" render={props => layout(props, BorealisTranscript, 'kaltura_video')} />
+          <Route path="/audio/transcript" render={props => layout(props, BorealisTranscript, 'audio')} />
+          <Route path="/video/transcript" render={props => layout(props, BorealisTranscript, 'video')} />
+          <Route path="/pdf/:id/transcript" render={props => layout(props, BorealisTranscript, 'pdf')} />
+          <Route path="/ppt/transcript" render={props => layout(props, BorealisTranscript, 'ppt')} />
         </Switch>
       </div>
     );
