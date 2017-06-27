48883c4f7cdb3ea1c2998fb474993e03a5d76618
chadfennell <libsys@gmail.com>
2017-06-27 09:38:12 -0700

    using the -p flag eliminates the need to explicitly uglify, cleans up webpack config

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -16,23 +16,6 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false,
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true,
-      },
-      compress: {
-        screw_ie8: true,
-      },
-      comments: false,
-    }),
-  ],
   context: resolve('src'),
   entry: './react-borealis.js',
   devtool: 'source-map',
