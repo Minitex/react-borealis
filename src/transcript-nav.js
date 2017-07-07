1f0cb22084397aea1f1d3b2c075612336ced5ce8
chadfennell <libsys@gmail.com>
2017-07-07 12:54:07 -0500

    and more linting

--- src/transcript-nav.js
+++ src/transcript-nav.js
@@ -2,27 +2,41 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export default class TranscriptNav extends React.Component {
-    render() {
-      let { transcript, assetPath, transcriptPath } = this.props;
-      if (transcript.texts.length > 0) {
-          return (
-                    <div className="row image-nav">
-                      <ul className="nav nav-pills">
-                        <li><NavLink className="transcript-nav-link" activeClassName="active" to={assetPath}>{transcript.label}</NavLink></li>
-                        <li ><NavLink className="transcript-nav-link" activeClassName="active" to={transcriptPath}>Transcript</NavLink></li>
-                      </ul>
-                    </div>
-                  )
-        } else {
-          return (<span />)
-        }
-      }
-}
+const TranscriptNav = (props) => {
+  const { transcript, assetPath, transcriptPath } = props;
+  if (transcript.texts.length > 0) {
+    return (
+      <div className="row image-nav">
+        <ul className="nav nav-pills">
+          <li>
+            <NavLink
+              className="transcript-nav-link"
+              activeClassName="active"
+              to={assetPath}
+            >
+              {transcript.label}
+            </NavLink>
+          </li>
+          <li >
+            <NavLink
+              className="transcript-nav-link"
+              activeClassName="active"
+              to={transcriptPath}
+            >
+              Transcript
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  return (<span />);
+};
 
-const propTypes = {
+TranscriptNav.propTypes = {
   assetPath: PropTypes.string.isRequired,
   transcriptPath: PropTypes.string.isRequired,
-}
+  transcript: PropTypes.object.isRequired,
+};
 
-TranscriptNav.propTypes = propTypes
+export default TranscriptNav;
