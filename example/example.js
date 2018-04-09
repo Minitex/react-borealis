38cebd9a732b1d5a5ed18be624f89859039f975d
chadfennell <libsys@gmail.com>
2018-04-09 16:13:00 -0500

    use the ssl version

--- example/example.js
+++ example/example.js
@@ -170,7 +170,7 @@ const config =
       },
       values: [
         {
-          src: 'http://cdm16022.contentdm.oclc.org/utils/getfile/collection/p16022coll52/id/16/filename',
+          src: 'https://cdm16022.contentdm.oclc.org/utils/getfile/collection/p16022coll52/id/16/filename',
           thumbnail: 'https://cdm16022.contentdm.oclc.org/utils/getthumbnail/collection/p16022coll52/id/16',
           transcript: {
             texts: ['1 Cecelia Boone Narrator Sara Ring Interviewer June 9, 2011 Minitex Oral History Project Minneapolis, Minnesota SR: Tell us how you got started working at Minitex. How did you end up in the job you are doing right now? CB: As always, there is a story. blah blah'],
@@ -178,7 +178,7 @@ const config =
           },
         },
         {
-          src: 'http://cdm16022.contentdm.oclc.org/utils/getfile/collection/p16022coll52/id/17/filename',
+          src: 'https://cdm16022.contentdm.oclc.org/utils/getfile/collection/p16022coll52/id/17/filename',
           thumbnail: 'https://cdm16022.contentdm.oclc.org/utils/getthumbnail/collection/p16022coll52/id/17',
           transcript: {
             texts: ['blah blah blah, second pdf here'],
